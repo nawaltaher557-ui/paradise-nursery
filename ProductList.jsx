ffea075eded138b154c76091a9258834ae405c15
nawaltaher557-ui <nawaltaher557@gmail.com>
2026-09-21 12:19:19 +0300

    Create ProductList.jsx

---
+++ ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductList.css';
+
+function ProductList() {
+  return (
+    <div className="product-list-container">
+      <h2>Our Plants Collection</h2>
+    </div>
+  );
+}
+
+export default ProductList;
