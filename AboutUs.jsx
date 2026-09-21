cf81e3014a29dff7e4439b4a235f65b8464a2e66
nawaltaher557-ui <nawaltaher557@gmail.com>
2026-09-21 12:12:39 +0300

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity...</p>
+    </div>
+  );
+}
+
+export default AboutUs;
